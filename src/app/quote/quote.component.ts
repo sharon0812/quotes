85c8ba22e8990992630bff4af18cbeefa329ffc3
sharon0812 <anyangosharon@gmail.com>
2020-11-07 11:36:36 +0300

    add pipe component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,10 +8,10 @@ import {Quote} from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote( 'Push yourself, because no one else is going to do it for you',  'Sharon Anyango', 'Mary', new Date(2015,12,5)),
-    new Quote('Whatever i am or ever hope to be i owe it to my mother', 'Abraham Lincon ', 'Jane', new Date(2019,08,10)),
-    new Quote('Work hard in silence , let your succes shout', 'Jack Ma', 'Gianna', new Date(2020,10, 15)),
-    new Quote('The harder you work for something ,the greater you will fill when you achieve it', 'Keylee Vee', 'Favour',new Date(2016,11,12)),
+    new Quote( 'Push yourself, because no one else is going to do it for you',  'Sharon Anyango', 'Mary', new Date()),
+    new Quote('Whatever i am or ever hope to be i owe it to my mother', 'Abraham Lincon ', 'Jane', new Date()),
+    new Quote('Work hard in silence , let your succes shout', 'Jack Ma', 'Gianna', new Date()),
+    new Quote('The harder you work for something ,the greater you will fill when you achieve it', 'Keylee Vee', 'Favour',new Date()),
 
   ];   
   addNewQuote (quote){
